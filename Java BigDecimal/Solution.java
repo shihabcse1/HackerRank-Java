276ac22353b7ee48e9361365f6cbd810563de05e
C-1/2 <tomdshihab@gmail.com>
2020-05-20 12:38:45 +0600

    Java 8

---
+++ Java BigDecimal/Solution.java
@@ -0,0 +1,32 @@
+//Problem link : https://www.hackerrank.com/challenges/java-bigdecimal/problem
+
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+        Arrays.sort(s, 0, n, new Comparator<Object>() {
+            public int compare(Object o1, Object o2) {
+                BigDecimal bd1 = new BigDecimal((String)o1);
+                BigDecimal bd2 = new BigDecimal((String)o2);
+                return bd2.compareTo(bd1);
+            }
+        });        //Write your code here
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
